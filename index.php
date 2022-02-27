2e2061dfaea461a8487e4b6f54341ab501c28477
jheey05 <100351606+jheey05@users.noreply.github.com>
2022-02-28 01:08:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Employee Payroll</title>
+        <link rel="stylesheet" href="homestyle.css">
+    </head>
+    <body>
+        <nav>
+            <label class="logo">ABCS Institute</label>
+        </nav>
+
+
+<div class="container">
+    <h1>Welcome to ABCS Institute</h1>
+
+    <a href="/teachingemployee_record.php">
+        <button class="btn btn1" >Teaching Employee</button>
+    </a>
+
+    <a href="nonteachingemployee_record.php">
+    <button class="btn btn2">Non-Teaching Employee</button>
+    </a>
+
+</div>
+    </body>
+</html>
